f3b61d1d1e20cf8215593119a1a19ba84ce03cb0
Md Asaduzzaman Anik <asaduzzamananik12@gmail.com>
2025-11-30 05:30:12 +0600

    Added Fetch Error Handlers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,19 +4,29 @@ import Bloglist from "./Bloglist";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
     fetch("http://localhost:3000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("Invalid URL to fetch data from");
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
         setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <div className="home p-10">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <Bloglist blogs={blogs} title="All blogs" />}
     </div>
