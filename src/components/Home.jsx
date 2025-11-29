7408adc9959ea86fd2ab15c1aa025f24230ec6ab
Md Asaduzzaman Anik <asaduzzamananik12@gmail.com>
2025-11-29 23:54:09 +0600

    Used Mapping Function to output list elements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("Anik");
-  const [age, setAge] = useState(30);
-
-  //   click event handler for button
-  const handleClick = () => {
-    setName("Muntaha");
-    setAge(24);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "First Blog",
+      body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque minima harum sequi molestias sunt doloribus deleniti placeat voluptates perferendis eum qui eveniet, maiores at magnam quod, esse aliquam numquam nostrum!",
+      author: "Anik",
+      id: 1,
+    },
+    {
+      title: "Second Blog",
+      body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      author: "Muntaha",
+      id: 2,
+    },
+    {
+      title: "Third Blog",
+      body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque minima harum sequi molestias sunt doloribus deleniti placeat voluptates perferendis eum qui eveniet, maiores at magnam quod, esse aliquam numquam nostrum!",
+      author: "Anik",
+      id: 1,
+    },
+  ]);
 
   return (
-    <div className="home p-5">
-      <h2 className="text-3xl font-medium py-5">Homepage</h2>
-      <p>
-        {name} is {age} years old.
-      </p>
-      <button
-        className="cursor-pointer bg-pink-400 hover:bg-pink-500 rounded-lg px-2 py-1 my-2 font-medium"
-        onClick={handleClick}
-      >
-        Click Me
-      </button>
+    <div className="home p-10">
+      <h2 className="text-2xl font-medium pb-5 uppercase">
+        Here are some blogs
+      </h2>
+      {blogs.map((blog) => (
+        <div
+          className="blog-preview shadow shadow-pink-300 mb-3 p-2 rounded-lg"
+          key={blog.id}
+        >
+          <h2 className="text-2xl font-medium text-pink-500">{blog.title}</h2>
+          <p className="text-lg font-medium">Author: {blog.author}</p>
+          <p className="border-t border-t-pink-400">{blog.body}</p>
+        </div>
+      ))}
     </div>
   );
 };
