845350b1e9116d6383af0ca20536899ef05af053
Md Asaduzzaman Anik <asaduzzamananik12@gmail.com>
2025-11-30 03:45:17 +0600

    Delete Blog Feature Added in This Branch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,21 +19,17 @@ const Home = () => {
       title: "Third Blog",
       body: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque minima harum sequi molestias sunt doloribus deleniti placeat voluptates perferendis eum qui eveniet, maiores at magnam quod, esse aliquam numquam nostrum!",
       author: "Anik",
-      id: 1,
+      id: 3,
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
   return (
     <div className="home p-10">
-      <Bloglist blogs={blogs} title="All blogs" />
-      <Bloglist
-        blogs={blogs.filter((blog) => blog.author === "Anik")}
-        title="Anik's blogs"
-      />
-      <Bloglist
-        blogs={blogs.filter((blog) => blog.author === "Muntaha")}
-        title="Muntaha's blogs"
-      />
+      <Bloglist blogs={blogs} title="All blogs" handleDelete={handleDelete} />
     </div>
   );
 };
