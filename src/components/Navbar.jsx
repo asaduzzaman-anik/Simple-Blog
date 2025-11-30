51aef7be4e844dfb40a2dcdbc809f5dc29bf71b5
Md Asaduzzaman Anik <asaduzzamananik12@gmail.com>
2025-12-01 01:04:39 +0600

    Created Router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between items-center px-20 py-5'>
-        <h2 className='text-2xl font-medium text-pink-500 text-nowrap flex-1'>
-            Anik's Blog❤️
-        </h2>
-        <div className='flex justify-between items-center gap-4 text-lg font-medium'>
-            <a href="" className='px-2 hover:underline hover:text-pink-500'>Home</a>
-            <a href="" className='px-2 hover:underline hover:text-pink-500'>Create Blog</a>
-        </div>
+    <nav className="flex justify-between items-center px-20 py-5">
+      <h2 className="text-2xl font-medium text-pink-500 text-nowrap flex-1">
+        Anik's Blog❤️
+      </h2>
+      <div className="flex justify-between items-center gap-4 text-lg font-medium">
+        <a
+          href=""
+          className="px-2 hover:underline hover:underline-offset-5 hover:text-pink-500"
+        >
+          Home
+        </a>
+        <a
+          href=""
+          className="px-2 hover:underline hover:underline-offset-5 hover:text-pink-500"
+        >
+          Create Blog
+        </a>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
